278515e57d8d536813514c1c433b38440a57a44d
Joaquin <joaquinmartinezfranco@gmail.com>
2023-11-19 14:50:45 -0300

    entrtopia

--- tp4.py
+++ tp4.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def leeArchivo(archivo):
     with open(archivo, "r") as f:
@@ -6,26 +7,45 @@ def leeArchivo(archivo):
 
         if linea :
             probabilidades_simbolos = linea.split()   
-            probabilidades_simbolos = [float(probabilidad) for probabilidad in probabilidades_simbolos]
-        for linea in f:
+            probabilidades_simbolos = [float(probabilidad) for probabilidad in probabilidades_simbolos] # primero lee probabilidad de 1 y 0
+     
+        for linea in f: #probabilidad de que salga un simbolo despues de otro simbolo
             linea = linea.split()
             linea = [float(valor) for valor in linea]
             matriz.append(linea)
 
-        print(probabilidades_simbolos)
-        print(matriz)
         return probabilidades_simbolos, matriz
 
 def simular_mensajes(matriz,probabilidades_simbolos,N,M):
     mensaje=[]
     for i in range(N):
-        mensaje.append(np.random.choice(len(probabilidades_simbolos),M,probabilidades_simbolos))
-        print(mensaje)
+        mensaje.append(np.random.choice(len(probabilidades_simbolos),M,probabilidades_simbolos)) #Crea N mensajes de longitud M
+
     return mensaje
 
 matriz=[]
-probabilidades_simbolos,matriz=leeArchivo("tp4_sample0.txt")
 
-n_mensajes=3
-m_mensajes=4
-simulacion_mensajes=simular_mensajes(matriz,probabilidades_simbolos,n_mensajes,m_mensajes)
+def calcula_entropia_canal(matriz,probabilidades_simbolos):
+    entropia_canal=0
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            if(matriz[i][j]!=0):
+                xxx entropia_canal+=matriz[i][j]*probabilidades_simbolos[i]*np.log2(1/matriz[i][j])
+    return entropia_canal
+
+def calcula_equivocacion_canal(matriz,probabilidades_simbolos):
+
+
+if (len(sys.argv) ==4 or len(sys.argv) ==5):
+    probabilidades_simbolos,matriz=leeArchivo(sys.argv[1]) #Punto a
+    n_mensajes=int(sys.argv[2])                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    m_mensajes=int(sys.argv[3])
+    flag_paridad_cruzada= ((len(sys.argv) == 5) and (sys.argv[4]=="-p"))
+
+
+#   P00=P(b=0/a=0) P01=P(b=1/a=0) 
+#   P10=P(b=0/a=1) P11=P(b=1/a=1)
+
+    simulacion_mensajes=simular_mensajes(matriz,probabilidades_simbolos,n_mensajes,m_mensajes)
+
+
